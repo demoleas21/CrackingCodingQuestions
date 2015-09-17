a01d33da94e9fbfd6cb19db5bc95f1f8bb40fdae
demoleas21 <andrew.demoleas@gmail.com>
2015-09-16 21:53:44 -0400

    optimized code

--- IsUnique.py
+++ IsUnique.py
@@ -1,11 +1,17 @@
 """ 1.1 Is Unique """
 
 def uniqueCheck(string):
-    for x in range (0, len(string)):
+    sortedStr = sorted(string)
+    for x in range(0,len(string)-1):
+        if sortedStr[x] == sortedStr[x+1]:
+            return False
+    return True
+
+    """for x in range (0, len(string)):
         for y in range (x+1, len(string)):
             if string[x] == string[y]:
                 return False
-    return True
+    return True"""
 
 string = 'andrew'
 print(uniqueCheck(string)) # Should return True
