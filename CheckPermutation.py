794b0e263faf153eea1ea5e2a1dc47b90c6e80c5
demoleas21 <andrew.demoleas@gmail.com>
2015-09-20 16:55:17 -0400

    optimized CheckPerm

--- CheckPermutation.py
+++ CheckPermutation.py
@@ -5,6 +5,22 @@ from unittest import TestCase
 def permCheck(stringA, stringB):
     if len(stringA) != len(stringB):
         return False
+
+    stringCount = {}
+    for val in stringA:
+        try:
+            stringCount[val] = stringCount[val] + 1
+        except:
+            stringCount[val] = 1
+            
+    for val in stringB:
+        if stringCount[val] == 0:
+            return False
+        else:
+            stringCount[val] = stringCount [val] - 1
+    return True
+
+    """    
     count = 0
     for i in range(0, len(stringB)):
         for j in range(0, len(stringA)):
@@ -14,6 +30,7 @@ def permCheck(stringA, stringB):
         return True
     else:
         return False
+    """
 
 class isPermutationsTrue(TestCase):
     def testPermIsTrue(self):
