0d67a553affc3e0cb2ab89ff3cdccdd33ff36e3f
demoleas21 <andrew.demoleas@gmail.com>
2015-11-10 22:24:21 -0500

    redone

--- src/Chapter 2/RemoveDups.py
+++ src/Chapter 2/RemoveDups.py
@@ -1,47 +1,48 @@
 """ 2.1 Remove Dups """
 
-class LinkedList(object):
-    def __init__(self, head=None):
-        self.head = head
+class Node(object):
+    def __init__ (self, d, n = None):
+        self.data = d
+        self.next_node = n
+    def get_next (self):
+        return self.next_node
+    def set_next (self):
+        self.next_node = n
+    def get_data (self):
+        return self.data
+    def set_data (self):
+        self.data = d
 
-    def insert(self, data):
-        new_node = Node(data)
-        new_node.set_next(self.head)
-        self.head = new_node
-
-    def size(self):
-        current = self.head
-        count = 0
-        while current:
-            count += 1
-            current = current.get_next()
-        return count
-
-    def search(self, data):
-        current = self.head
-        found = False
-        while current and found is False:
-            if current.get_data() == data:
-                found = True
+class LinkedList (object):
+    def __init__ (self, r = None):
+        self.root = r
+        self.size = 0
+    def get_size (self):
+        return self.size
+    def add (self, d):
+        new_node = Node (d, self.root)
+        self.root = new_node
+        self.size += 1
+    def remove (self, d):
+        this_node = self.root
+        prev_node = None
+        while this_node:
+            if this_node.get_data() == d:
+                if prev_node:
+                    prev_node.set_next(this_node.get_next())
+                else:
+                    self.root = this_node
+                self.size -= 1
+                return True
             else:
-                current = current.get_next()
-        if current is None:
-            raise ValueError("Data not in list")
-        return current
+                prev_node = this_node
+                this_node = this_node.get_next()
+        return False
 
-    def delete(self, data):
-        current = self.head
-        previous = None
-        found = False
-        while current and found is False:
-            if current.get_data() == data:
-                found = True
-            else:
-                previous = current
-                current = current.get_next()
-        if current is None:
-            raise ValueError("Data not in list")
-        if previous is None:
-            self.head = current.get_next()
-        else:
-            previous.set_next(current.get_next())
+MyList = LinkedList()
+MyList.add(5)
+MyList.add(8)
+MyList.add(12)
+print(MyList.remove(12))
+print(MyList.remove(4))
+print(MyList.remove(12))
