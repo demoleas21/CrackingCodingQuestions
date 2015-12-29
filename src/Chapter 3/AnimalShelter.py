529c5dc5dced33597eac732c117066b0fd9a664d
demoleas21 <andrew.demoleas@gmail.com>
2015-12-28 21:39:09 -0500

    Shelter completed

--- src/Chapter 3/AnimalShelter.py
+++ src/Chapter 3/AnimalShelter.py
@@ -11,7 +11,31 @@ class Queue(object):
 
     def pop(self):
         """Removes the top element"""
-        self.container.pop(0)
+        animal = (self.container[0])
+        del self.container[0]
+        return animal
+
+    def pop_cat(self):
+        """Removes the top cat"""
+        i = 0
+        while i < len(self.container):
+            for animal in self.container[i]:
+                if animal == 'cat':
+                    cat = (self.container[i])
+                    del self.container[i]
+                    return cat
+            i = i + 1
+
+    def pop_dog(self):
+        """Removes the top dog"""
+        i = 0
+        while i < len(self.container):
+            for animal in self.container[i]:
+                if animal == 'dog':
+                    dog = (self.container[i])
+                    del self.container[i]
+                    return dog
+            i = i + 1
 
     def peek(self):
         """Display the top of queue"""
@@ -24,3 +48,28 @@ class Queue(object):
     def is_empty(self):
         """Informs if stack is empty"""
         return self.container == []
+
+def main():
+    """Main function"""
+    animal_shelter = Queue()
+    animal_shelter.push({'cat':'Tammy'})
+    animal_shelter.push({'dog':'Sir Barks-a-lot'})
+    animal_shelter.push({'dog':'Sparky'})
+    animal_shelter.push({'dog':'Coconut'})
+    animal_shelter.push({'cat':'Fluffy'})
+    animal_shelter.push({'cat':'Mustache'})
+    animal_shelter.push({'dog':'Snoopy'})
+
+    animal = input("\nWould you like a cat, dog, or either? ")
+    if animal == 'cat':
+        print(animal_shelter.pop_cat())
+    elif animal == 'dog':
+        print(animal_shelter.pop_dog())
+    elif animal == 'either':
+        print(animal_shelter.pop())
+    else:
+        print("Error! Please try again")
+
+
+if __name__ == "__main__":
+    main()
